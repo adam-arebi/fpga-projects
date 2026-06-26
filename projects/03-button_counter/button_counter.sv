ef3aef372f908f0f2c53eac791baf98e63670a2e
Adam Arebi <238073627+adam-arebi@users.noreply.github.com>
2026-06-25 22:11:19 -0400

    added debouncing

--- projects/03-button_counter/button_counter.sv
+++ projects/03-button_counter/button_counter.sv
@@ -7,11 +7,16 @@ module button_counter(
 );
 
 logic ff1, synced;
-logic synced_prev;
+logic clean_prev;
 logic press_pulse;
 logic [1:0] count;
 assign LEDR_N = ~count [0];
 assign LEDG_N = ~count [1];
+
+localparam DB_MAX = 120_000; //enough for around 10 ms with 12MHz
+logic clean;
+logic [16:0] db_count; //enough to holds DB_MAX
+
 
 //synchronizer
 always_ff@(posedge CLK)
@@ -23,10 +28,10 @@ end
 //edge trigger 
 always_ff@(posedge CLK)
 begin
-    synced_prev <= synced; //make synced_prev just whatever synced was at clk edge and it holds it
+    clean_prev <= clean; //make synced_prev just whatever synced was at clk edge and it holds it
 end
 
-assign press_pulse = synced & ~synced_prev;
+assign press_pulse = clean & ~clean_prev;
 
 //counter, gated by the pulse
 always_ff@(posedge CLK)
@@ -34,6 +39,25 @@ begin
     if (press_pulse)
     begin
         count <= count+1;
+    end
+end
+
+
+//debouncer
+
+always_ff@(posedge CLK)
+begin
+    if (synced == clean)
+    begin
+        db_count <= 0;
+    end
+    else
+    begin
+        db_count <= db_count + 1;
+        if (db_count == DB_MAX)
+        begin
+            clean <= synced;
+        end
     end
 end
 
